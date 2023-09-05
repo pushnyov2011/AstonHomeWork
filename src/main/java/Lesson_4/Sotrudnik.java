90b528497af9306c425cbc2e7bef5afee3273379
pushn <pushnyov2011@yandex.by>
2023-09-05 10:15:10 +0300

    урок 4

--- src/main/java/Lesson_4/Sotrudnik.java
+++ src/main/java/Lesson_4/Sotrudnik.java
@@ -111,30 +111,6 @@ public class Sotrudnik {
         }
 
         // public  Sotrudnik ()
-
-
-        public  static void main (String[] args)
-        {
-            Sotrudnik person = new Sotrudnik();
-            person.print_info();
-
-            Sotrudnik person2 = new Sotrudnik("ТЕст","Qa", "yander@ys.ru", 54564546, 456.6 ,80);
-            person2.print_info();
-
-            Sotrudnik person3 = new Sotrudnik();
-
-
-
-
-        }
-
-
-    /*
-    нутри класса «Сотрудник» написать метод, который выводит информацию об
-объекте в консоль.
-
-     */
-
         public  void  print_info()
         {
             System.out.println("Досье на сотрудника  ");
@@ -147,6 +123,63 @@ public class Sotrudnik {
 
         }
 
+    public static void task2() {
+        Sotrudnik[] person = new Sotrudnik[5]; //
+        person[0] = new Sotrudnik("Первый работник", "Ломастер", "test@ya.ru", 12345679, 5, 50);
+        person[0].print_info();
+        System.out.println();
+        // хотел посмотреть как будет работать через геттер и сеттар работать
+        person[1] = new Sotrudnik();
+        person[1].setFIO("Сотрудник два");
+        person[1].setKem_rabotaet("Ломастиер высшей категории");
+        person[1].setEmail("test202@ya.ru");
+        person[1].setZp(-9);
+        person[1].setTelephone(-5645465);
+        person[1].setAge(-2);
+        person[1].print_info();
+        person[2] = new Sotrudnik("Третий работник", "Дворник", "dvor@ya.ru", 12305679, 5456, 90);
+        System.out.println();
+        person[2].print_info();
+        person[3] = new Sotrudnik("Четверный работник", "ПРосто КОТ", "cat@ya.ru", 911, 2, 42);
+        System.out.println();
+        person[3].print_info();
+        person[4] = new Sotrudnik();
+        System.out.println();
+        person[4].print_info();
+        System.out.println("\nСотрудники старше 40 лет.\n");
 
+        // С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+
+        for (int i = 0; i < person.length; i++) {
+            if (person[i].getAge() > 40) {
+                System.out.println("Сотрудник: " + (i + 1));
+                person[i].print_info();
+                System.out.println();
+            }
+        }
     }
+
+
+        public  static void main (String[] args)
+        {
+            Sotrudnik person = new Sotrudnik();
+            person.print_info();
+
+            Sotrudnik person2 = new Sotrudnik("ТЕст","Qa", "yander@ys.ru", 54564546, 456.6 ,80);
+            person2.print_info();
+
+            Sotrudnik person3 = new Sotrudnik();
+
+            //задание 2 Создать массив из 5 сотрудников0  С помощью цикла вывести информацию только о сотрудниках старше 40 лет
+            System.out.println("Задание 2 . Создать массив из 5 сотрудников.С помощью цикла вывести информацию только о сотрудниках старше 40 лет ");
+            task2();
+        }
+
+
+
+
+        }
+
+
+
 
