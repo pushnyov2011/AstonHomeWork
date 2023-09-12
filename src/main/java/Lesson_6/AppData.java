0fae0f1a56ff5e0f1152fe7dc2a801079053b87f
pushn <pushnyov2011@yandex.by>
2023-09-12 18:37:15 +0300

    урок 6

---
+++ src/main/java/Lesson_6/AppData.java
@@ -0,0 +1,23 @@
+package Lesson_6;
+
+public class AppData {
+    private  String [] headrer;
+    private  int [][] data;
+
+    public String[] getHeadrer()
+    {
+        return headrer;
+    }
+
+    public void setHeadrer(String[] headrer) {
+        this.headrer = headrer;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
+
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+}
